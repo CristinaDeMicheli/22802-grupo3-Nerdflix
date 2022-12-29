d6ccc465b136daca71e3248468b904a55a832499
Cristina De Micheli <43018839+CristinaDeMicheli@users.noreply.github.com>
2022-12-29 18:33:35 -0300

    cambios en los estilos

--- src/componentes/menu.js
+++ src/componentes/menu.js
@@ -1,38 +1,72 @@
 import { useAuth } from "../contexto/authContext";
-export function Menu(){ 
-    const {user, logout, loading} = useAuth()
-    const handleLogout = async() =>{
-       await logout();
-    };
-    if (loading) return <h1>cargando...</h1>
-    //const {user}=useAuth();
-    //console.log(user);
-    return <div>
-   <nav class="navbar navbar-expand-lg navbar-light bg-dark">
-  <a class="navbar-brand" href="http://localhost:3000/peliculas"><h1 class="text-start ms-3 mt-3" id="logo">NERDFLIX</h1></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+import BuscarPelicula from "./BuscarPelicula";
+import { Spinner } from "./Spinner";
+export function Menu() {
+  const { user, logout, loading } = useAuth();
+  const handleLogout = async () => {
+    await logout();
+  };
+  if (loading) return <Spinner />;
+  //const {user}=useAuth();
+  //console.log(user);
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <a className="navbar-brand" href="http://localhost:3000/peliculas">
+          <h1 className="text-start ms-3 mt-3" id="logo">
+            NERDFLIX
+          </h1>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="http://localhost:3000/peliculas">Peliculas</a>
-      </li>
-    </ul>
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="http://localhost:3000/comentarios">Comentarios</a>
-      </li>
-    </ul>
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="http://localhost:3000/contacto">Contacto</a>
-      </li>
-    </ul>
-   
-  </div>
-  <button onClick={handleLogout}>Cerrar sessión</button>
-</nav>  
-   </div>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a
+                className="nav-link text-white-50"
+                href="http://localhost:3000/peliculas"
+              >
+                Peliculas
+              </a>
+            </li>
+          </ul>
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a
+                className="nav-link text-white-50"
+                href="http://localhost:3000/comentarios"
+              >
+                Comentarios
+              </a>
+            </li>
+          </ul>
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a
+                className="nav-link text-white-50"
+                href="http://localhost:3000/contacto"
+              >
+                Contacto
+              </a>
+            </li>
+          </ul>
+         
+        
+        </div>
+        <button className="btn btn-danger  me-3 mt-3" onClick={handleLogout}>
+          Cerrar sesión
+        </button>
+      </nav>
+    </div>
+  );
 }
