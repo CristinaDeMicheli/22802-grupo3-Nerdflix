f128485d65b2f92e113a85f5f366ffaf511b83cc
Cristina De Micheli <43018839+CristinaDeMicheli@users.noreply.github.com>
2022-12-29 19:00:06 -0300

    paginas informativas del login

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import DefaultComponent from "./comentarios-usuarios/AppComentarios";
 import BuscarPelicula from "./componentes/BuscarPelicula";
 import Contacto from "./componentes/Contacto";
 import Peliculas from "./componentes/Peliculas";
+import Centro from "./componentes/Centro";
+import Cookies from "./componentes/Cookies";
+import Informacion from "./componentes/Informacion";
+import Preguntas from "./componentes/Preguntas";
 
 function App() {
   return (
@@ -20,6 +24,10 @@ function App() {
     }/> 
     <Route path='/login' element={<Login/>}/> 
     <Route path='/registro' element={<Registro/>}/> 
+    <Route path='/centro' element={<Centro/>}/> 
+    <Route path='/informacion' element={<Informacion/>}/> 
+    <Route path='/cookies' element={<Cookies/>}/> 
+    <Route path='/preguntas' element={<Preguntas/>}/> 
     
     <Route path='/peliculas' element={<ProtectedRoute><Peliculas /></ProtectedRoute>}/> 
     <Route path='/buscar-peliculas' element={<ProtectedRoute><BuscarPelicula /></ProtectedRoute>}/> 
